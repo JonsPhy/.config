f4f881fe99491921f86d5859bcfe73944d665bcf
jonasvonstein <j.vonstein.jvs@gmail.com>
2024-10-13 20:49:00 +0200

    add system preferences to bootstrap

--- bootstrap.sh
+++ bootstrap.sh
@@ -14,3 +14,25 @@ brew install --cask notion
 brew install --cask arc
 brew install --cask karabiner-elements
 brew install --cask warp
+
+
+
+
+defaults write com.apple.finder FXPreferredViewStyle -string "icnv"
+defaults write com.apple.finder DesktopViewSettings -dict-add IconViewSettings '{iconSize = 64; gridSpacing = 54; showIconPreview = 1;}'
+
+# Set List view settings
+defaults write com.apple.finder FXPreferredViewStyle -string "Nlsv"
+defaults write com.apple.finder StandardViewSettings -dict-add ListViewSettings '{iconSize = 16; useRelativeDates = 1;}'
+
+# Set sidebar width
+defaults write com.apple.finder SidebarWidth -int 159
+
+# Set the default new window target (e.g., iCloud Drive)
+defaults write com.apple.finder NewWindowTargetPath -string "file://${HOME}/Library/Mobile Documents/com~apple~CloudDocs/"
+
+# Arrange files by name in Finder
+defaults write com.apple.finder FXArrangeGroupViewBy -string "Name"
+
+# Restart Finder to apply changes
+killall Finder
